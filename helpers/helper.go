9ea6972f016c725b2f32408ad400f67e222a4407
james <james@legend.tech>
2024-01-04 21:54:34 +0800

    ui test 1

--- helpers/helper.go
+++ helpers/helper.go
@@ -1,6 +1,31 @@
-package helpers
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"github.com/james-ray/recovery-tool/common"
+)
 
 //export hello
 func hello() string {
 	return "Greetings"
+}
+
+//export generateChildExtendedPrivateKey
+func generateChildExtendedPrivateKey(metadataFilePath string, walletType int, vaultIndex int, chainInt int, subIndex int) string {
+	// 实现生成子扩展私钥的逻辑
+	hdPath := fmt.Sprintf("81/%d/%d/%d/%d", walletType, vaultIndex, chainInt, subIndex)
+	metadataMap, err := common.ReadMetadataFile(metadataFilePath)
+	if err != nil {
+		panic(err)
+	}
+	privBytes, err := common.DeriveChildPrivateKey(metadataMap, hdPath)
+	if err != nil {
+		panic(err)
+	}
+	return hex.EncodeToString(privBytes)
+}
+
+func main() {
+
 }
